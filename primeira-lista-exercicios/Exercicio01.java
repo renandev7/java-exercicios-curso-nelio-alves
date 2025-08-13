337298e50aa3551011152bd50c2efe63d4a44bb3
Renan <renancontatodevv@gmail.com>
2025-08-12 21:44:24 -0300

    Create Exercicio01.java

---
+++ primeira-lista-exercicios/Exercicio01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,resultado;
+
+        System.out.print("Digite o primeiro numero:");
+        n1 = sc.nextInt();
+
+        System.out.print("Digite o segundo numero:");
+        n2 = sc.nextInt();
+
+        resultado = n1+n2;
+
+        System.out.printf("Resultado: %d%n", resultado);
+
+        sc.close();
+    }
+}
+
+//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+//mensagem explicativa, conforme exemplos.
